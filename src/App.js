38ce1a02148823de9d199954a7009d3f7f018a0c
Prakash-sa <sainiprakash525@gmail.com>
2022-10-19 16:41:26 -0500

    react web image uploader

--- src/App.js
+++ src/App.js
@@ -1,23 +1,120 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { ResultImageCard } from './components/ResultImageCard';
+
+
+// best article https://blog.logrocket.com/create-drag-and-drop-component-react-dropzone/
+const imageTypeRegex = /image\/(png|jpg|jpeg)/gm;
 
 function App() {
+  const [imageFiles, setImageFiles] = useState([]);
+  const [images, setImages] = useState([]);
+
+  const uploadFiles = () => {
+    const formData = new FormData();
+    // console.log(imageSent);
+    // formData.append('image', imageSent);
+    formData.append('key', 'Your Api key goes here');
+    axios.post('https://api.imgbb.com/1/upload', formData).then((response) => {
+      console.log(response);
+    });
+  };
+
+  const changeHandler = (e) => {
+    const { files } = e.target;
+    const validImageFiles = [];
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      if (file.type.match(imageTypeRegex)) {
+        validImageFiles.push(file);
+      }
+    }
+    if (validImageFiles.length) {
+      setImageFiles(validImageFiles);
+      return;
+    }
+    alert("Selected images are not of valid type!");
+  };
+
+  
+
+  useEffect(() => {
+    const images = [], fileReaders = [];
+    let isCancel = false;
+    if (imageFiles.length) {
+      imageFiles.forEach((file) => {
+        const fileReader = new FileReader();
+        fileReaders.push(fileReader);
+        fileReader.onload = (e) => {
+          const { result } = e.target;
+          if (result) {
+            images.push(result)
+          }
+          if (images.length === imageFiles.length && !isCancel) {
+            setImages(images);
+          }
+        }
+        fileReader.readAsDataURL(file);
+      })
+    };
+    return () => {
+      isCancel = true;
+      fileReaders.forEach(fileReader => {
+        if (fileReader.readyState === 1) {
+          fileReader.abort()
+        }
+      })
+    }
+  }, [imageFiles]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <form>
+        <div class="mx-12 my-12">
+              <label class="block text-sm font-medium text-gray-700">Cover photo</label>
+              <div class="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
+                <div class="space-y-1 text-center">
+                  <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                    <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                  </svg>
+                  <div class="flex text-sm text-gray-600">
+                    <label for="file-upload" class="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500">
+                      <span>Upload a file</span>
+                      <input id="file-upload" name="file-upload" type="file" class="sr-only" onChange={changeHandler} accept="image/png, image/jpg, image/jpeg" multiple/>
+                    </label>
+                    <p class="pl-1">or drag and drop</p>
+                  </div>
+                  <p class="text-xs text-gray-500">PNG, JPG, JPEG</p>
+                </div>
+              </div>
+            <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+              <button type="submit" class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Save</button>
+            </div>
+          </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <label htmlFor="file">Upload images</label>
+          <input
+            type="file"
+            id="file"
+            onChange={changeHandler}
+            accept="image/png, image/jpg, image/jpeg"
+            multiple
+          />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </form>
+      {
+        images.length > 0 ?
+          <div>
+            {
+              images.map((image, idx) => {
+                return <p key={idx}> <ResultImageCard image={image}/> </p>
+              })
+            }
+          </div> : null
+      }
     </div>
   );
 }
